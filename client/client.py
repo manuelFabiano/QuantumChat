bd3ae23a86e2755ab5caff48fc84711b4fdedbbc
manuelFabiano <manuel.fabiano.36@gmail.com>
2024-06-22 01:26:30 +0200

    Added kyber keys and now upload works

--- client/client.py
+++ client/client.py
@@ -3,6 +3,10 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'kyberpy'))
+from kyberpy import kyber
 import json
 
 #Server URL
@@ -45,13 +49,16 @@ def export_keys(data):
         json.dump(data, file, cls=Ed25519PrivateKeyEncoder, indent=4)
 
 
-#genereate public and private keys
+#generate public and private keys
 def generate_keys():
+    # Mancano identificatori per le chiavi!!!
+    # Curve keys
     private_identity_key = Ed25519PrivateKey.generate()
     public_identity_key = private_identity_key.public_key()
     private_prekey = Ed25519PrivateKey.generate()
     public_prekey = private_prekey.public_key()
-    sign_on_prekey = private_prekey.sign(public_prekey.public_bytes(
+    # La firma deve essere fatta con la identity key?
+    sign_on_prekey = private_identity_key.sign(public_prekey.public_bytes(
     encoding=serialization.Encoding.Raw,
     format=serialization.PublicFormat.Raw
     ))
@@ -61,11 +68,27 @@ def generate_keys():
         private_one_time_prekeys.append(Ed25519PrivateKey.generate())
         public_one_time_prekeys.append(private_one_time_prekeys[i].public_key())
 
-    #Json Data structure containing private keys
+    # Kyber keys:
+    private_last_resort_pqkem_key , public_last_resort_pqkem_kyber_key = kyber.Kyber512.keygen()
+    sign_on_last_resort_pqkem_key = private_identity_key.sign(public_last_resort_pqkem_kyber_key)
+
+    private_one_time_pqkem_prekeys = list()
+    public_one_time_pqkem_prekeys = list()
+    sign_on_one_time_pqkem_prekeys = list()
+
+    for i in range(5):
+        pqkem = kyber.Kyber512.keygen()
+        private_one_time_pqkem_prekeys.append(pqkem[0])
+        public_one_time_pqkem_prekeys.append(pqkem[1])
+        sign_on_one_time_pqkem_prekeys.append(private_identity_key.sign(pqkem[1]))
+
+    # Json Data structure containing private keys
     private_keys = {
         "private_identity_key": private_identity_key,
         "private_prekey": private_prekey,
-        "private_one_time_prekeys" : private_one_time_prekeys 
+        "private_one_time_prekeys" : private_one_time_prekeys,
+        "private_last_resort_pqkem_key" : str(private_last_resort_pqkem_key),
+        "private_one_time_pqkem_prekeys" : str(private_one_time_pqkem_prekeys)
     }
 
     public_keys = {
@@ -78,7 +101,11 @@ def generate_keys():
         "sign_on_prekey" : sign_on_prekey,
         "public_one_time_prekeys" : [key.public_bytes(
         encoding=serialization.Encoding.Raw,
-        format=serialization.PublicFormat.Raw) for key in public_one_time_prekeys ]
+        format=serialization.PublicFormat.Raw) for key in public_one_time_prekeys ],
+        "public_last_resort_pqkem_key" : public_last_resort_pqkem_kyber_key,
+        "sign_on_last_resort_pqkem_key" : sign_on_last_resort_pqkem_key,
+        "public_one_time_pqkem_prekeys" : [key for key in public_one_time_pqkem_prekeys],
+        "sign_on_one_time_pqkem_prekeys" : [sign for sign in sign_on_one_time_pqkem_prekeys]
     }
 
     export_keys(private_keys)
@@ -89,18 +116,27 @@ def generate_keys():
 
 def register(username, password,public_keys):
     url = SERVER + "/register"
+    # Mancano gli identifier delle chiavi?
     payload = {
         "username": username,
         "password": str(password),
-        "public_identity_key": str(public_keys["public_identity_key"]),
-        "public_prekey" : str(public_keys["public_prekey"]),
-        "sign_on_prekey" : str(public_keys["sign_on_prekey"]),
-        "public_one_time_prekeys" : [str(key) for key in public_keys["public_one_time_prekeys"] ]
+        "public_keys": {
+            "public_identity_key": str(public_keys["public_identity_key"]),
+            "public_prekey" : str(public_keys["public_prekey"]),
+            "sign_on_prekey" : str(public_keys["sign_on_prekey"]),
+            "public_one_time_prekeys" : [str(key) for key in public_keys["public_one_time_prekeys"] ],
+            "public_last_resort_pqkem_key" : str(public_keys["public_last_resort_pqkem_key"]),
+            "sign_on_last_resort_pqkem_key" : str(public_keys["sign_on_last_resort_pqkem_key"]),
+            "public_one_time_pqkem_prekeys" : [str(key) for key in public_keys["public_one_time_pqkem_prekeys"]],
+            "sign_on_one_time_pqkem_prekeys" : [str(sign) for sign in public_keys["sign_on_one_time_pqkem_prekeys"]]
+        }
     }
+    payload = json.dumps(payload, indent=4)
     response = requests.post(url, payload,headers = {"Content-Type": "application/json", "Accept": "application/json"})
     return response
 
 def main():
+    
     print("Welcome to QuantumChat!")
 
     username = input("Enter your username: ")
@@ -110,14 +146,21 @@ def main():
     digest = hashes.Hash(hashes.SHA256())
     digest.update(password.encode())
     password = digest.finalize()
+    
+    
     public_keys = generate_keys()
 
-    
     #Register the user
     response = register(username, password, public_keys)
 
-    print(response)
-
+    print(response.text)
 
 if __name__ == "__main__":
     main()
+
+'''
+pk, sk = kyber.Kyber512.keygen()
+c, key = kyber.Kyber512.enc(pk)
+_key = kyber.Kyber512.dec(c, sk)
+print(key == _key)
+'''
--- server/server.py
+++ server/server.py
@@ -24,6 +24,10 @@ def register():
         public_prekey= data["public_keys"]["public_prekey"]
         sign_on_prekey= data["public_keys"]["sign_on_prekey"]
         public_one_time_prekeys= data["public_keys"]["public_one_time_prekeys"]
+        public_last_resort_pqkem_key = data["public_keys"]["public_last_resort_pqkem_key"]
+        sign_on_last_resort_pqkem_key = data["public_keys"]["sign_on_last_resort_pqkem_key"]
+        public_one_time_pqkem_prekeys = data["public_keys"]["public_one_time_pqkem_prekeys"]
+        sign_on_one_time_pqkem_prekeys = data["public_keys"]["sign_on_one_time_pqkem_prekeys"]
 
         #Check if the username is already registered
         if keys_collection.find_one({'username': username}):
@@ -33,71 +37,25 @@ def register():
         keys_collection.insert_one({
             'username': username,
             'password': password,
-            "public_identity_key": public_identity_key,
-            "public_prekey" : public_prekey,
-            "sign_on_prekey" : sign_on_prekey,
-            "public_one_time_prekeys" : public_one_time_prekeys
+            "public_keys":{
+                "public_identity_key": public_identity_key,
+                "public_prekey" : public_prekey,
+                "sign_on_prekey" : sign_on_prekey,
+                "public_one_time_prekeys" : public_one_time_prekeys,
+                "public_last_resort_pqkem_key" : public_last_resort_pqkem_key,
+                "sign_on_last_resort_pqkem_key" : sign_on_last_resort_pqkem_key,
+                "public_one_time_pqkem_prekeys" : public_one_time_pqkem_prekeys,
+                "sign_on_one_time_pqkem_prekeys" : sign_on_one_time_pqkem_prekeys
+            }
         })
 
-        return jsonify({'message': 'Successly registered user!'}), 200
+        return jsonify({'message': 'Successfully registered user!'}), 200
 
     except KeyError as e:
         return jsonify({'error': 'Wrong data format: {}'.format(str(e))}), 400
 
-@app.route('/upload', methods=['POST'])
-def upload_keys():
-    data = request.get_json()
-
-    try:
-        #Bob's curve identity key
-        identity_public_key = binascii.unhexlify(data['identity_public_key'])
-        
-        #Bob's signed curve prekey and its identifier
-        signed_curve_prekey = binascii.unhexlify(data['signed_curve_prekey'])
-        identifier_curve_prekey = data['identifier_curve_prekey']
-
-        #Bob's signature on the curve prekey
-        signature_curve_prekey = binascii.unhexlify(data['signature_curve_prekey'])
-
-        #Bob's signed last-resort pqkem prekey and its identifier
-        signed_pqkem_prekey = binascii.unhexlify(data['signed_pqkem_prekey'])
-        identifier_pqkem_prekey = data['identifier_pqkem_prekey']
-
-        #Bob's signature on the pqkem prekey
-        signature_pqkem_prekey = binascii.unhexlify(data['signature_pqkem_prekey'])
-
-        #A set of Bob's one-time curve prekeys with identifiers
-        one_time_curve_prekeys = [{
-            'key': binascii.unhexlify(prekey['key']),
-            'id': prekey['id']
-        } for prekey in data['one_time_curve_prekeys']]
-
-        #A set of Bob's signed one-time pqkem prekeys with identifiers
-        one_time_pqkem_prekeys = [{
-            'key': binascii.unhexlify(prekey['key']),
-            'id': prekey['id']
-        } for prekey in data['one_time_pqkem_prekeys']]
-
-
-        #Save keys on MongoDB
-        #TODO: Da cambiare in modo che venga salvato nel documento (già esistente) dell'utente
-        keys_collection.insert_one({
-            'identity_public_key': identity_public_key,
-            'signed_curve_prekey': signed_curve_prekey,
-            'identifier_curve_prekey': identifier_curve_prekey,
-            'signature_curve_prekey': signature_curve_prekey,
-            'signed_pqkem_prekey': signed_pqkem_prekey,
-            'identifier_pqkem_prekey': identifier_pqkem_prekey,
-            'signature_pqkem_prekey': signature_pqkem_prekey,
-            'one_time_curve_prekeys': one_time_curve_prekeys,
-            'one_time_pqkem_prekeys': one_time_pqkem_prekeys
-        })
-
-
-        return jsonify({'message': 'Successly uploaded key bundle!'}), 200
-
-    except (KeyError, binascii.Error) as e:
-        return jsonify({'error': 'Wrong data format: {}'.format(str(e))}), 400
 
 if __name__ == '__main__':
+    print('Server running on port 5000')
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
