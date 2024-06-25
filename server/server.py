5d5057772cbc0e17bef23e739bdf127df4a86ea3
manuelFabiano <manuel.fabiano.36@gmail.com>
2024-06-25 13:03:27 +0200

    menu

--- server/server.py
+++ server/server.py
@@ -55,6 +55,8 @@ def register():
         return jsonify({'error': 'Wrong data format: {}'.format(str(e))}), 400
 
 
+
+
 if __name__ == '__main__':
     print('Server running on port 5000')
     app.run(debug=True, host='0.0.0.0', port=5000)
